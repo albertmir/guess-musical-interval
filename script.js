1208c126c78a2e354a36e8d76197f8c06cefecaa
Albert Mir <albert.mir@volcanicinternet.com>
2024-02-26 09:18:51 +0100

    feat: add some counters and avg time

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', generateQuestion);
 
+let correctCount = 0;
+let wrongCount = 0;
+let totalResponseTime = 0;
+let questionCount = 0;
+let startTime;
+
 const notesEs = ['DO', 'RE', 'MI', 'FA', 'SOL', 'LA', 'SI'];
 const notesEn = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 let notes = [...notesEs];
@@ -8,6 +14,10 @@ const userResponse = document.getElementById('userResponse');
 const verifyButton = document.getElementById('verifyButton');
 const questionElement = document.getElementById('question');
 const responseMessage = document.getElementById('responseMessage');
+const correctCountElement = document.getElementById('correctCount');
+const wrongCountElement = document.getElementById('wrongCount');
+const averageTimeElement = document.getElementById('averageTime');
+
 
 function toggleScale() {
   notes = document.getElementById('scaleToggle').checked
@@ -37,10 +47,15 @@ function generateQuestion() {
     questionElement.innerHTML = `¿Qué nota hay a la <b>${direction}</b> de la nota <span class="bg-gray-700 text-white p-1 mr-0.5">${selectedNote}</span>?`;
     questionElement.dataset.response = notes[responseIndex];
     questionElement.classList.remove('fade-animation');
+    startTime = Date.now();
   }, 250);
 }
 
 function verifyResponse() {
+  const responseTime = Date.now() - startTime;
+  totalResponseTime += responseTime;
+  questionCount++;
+
   const isCorrect =
     userResponse.value.toUpperCase().trim() ===
     questionElement.dataset.response;
@@ -51,18 +66,21 @@ function verifyResponse() {
   userResponse.value = '';
   userResponse.focus();
   if (isCorrect) {
+    correctCount++;
     pulseButtonSuccess();
     generateQuestion();
     setTimeout(() => {
       resetMessage();
     }, 1000);
   } else {
+    wrongCount++;
     shakeButtonError();
     setTimeout(() => {
       resetMessage();
     }, 1000);
     userResponse.select();
   }
+  updateUI();
 }
 
 function showMessage(message, isCorrect) {
@@ -98,6 +116,24 @@ function shakeButtonError() {
     verifyButton.classList.add('bg-blue-500', 'hover:bg-blue-700');
   }, 820);
 }
+
+function updateUI() {
+  correctCountElement.textContent = correctCount;
+  wrongCountElement.textContent = wrongCount;
+  const averageTimeSeconds =
+    questionCount > 0 ? totalResponseTime / questionCount / 1000 : 0;
+
+  let timeDisplay;
+  if (averageTimeSeconds >= 60) {
+    const averageTimeMinutes = averageTimeSeconds / 60;
+    timeDisplay = `${averageTimeMinutes.toFixed(2)}m`;
+  } else {
+    timeDisplay = `${averageTimeSeconds.toFixed(2)}s`;
+  }
+
+  averageTimeElement.textContent = timeDisplay;
+}
+
 
 function resetMessage() {
   responseMessage.innerHTML = '';
